e96eef7a54fb5353e27fe51114e2d354f890d98f
Nicholas Harding <nharding@csumb.edu>
2018-09-19 21:50:06 +0000

    Template of basic files.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <?php
+        include 'inc/functions.php';
+    ?>
+    
+    <head>
+        <title> Silverjack </title>
+        
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    
+    
+    <body>
+        
+        <div id="main">
+        <?php
+            
+        ?> <!--End of PHP code.-->
+        
+       
+        </div>
+
+    </body>
+    
+</html>
