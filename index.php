1036df366f35b19da50cdc27ba22c7fc05608c12
Nicholas Harding <nharding@csumb.edu>
2018-10-06 23:53:44 +0000

    Fixed the HTML referncing the css and made it a bit neater overall.

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
-<html><head>
-        <title>ilver Jack </title>
+<!DOCTYPE html>
+<html>
+    <?php
+        include 'inc/functions.php';
+    ?>
+    
+    <head>
+        <title> Silver Jack </title>
         
-        <link href="styles.css" rel="stylesheet" type="text/css" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
       
     </head>
    <body>
-       <div>
-    <header>
-        <h1>Silverjack</h1>
-    </header>
+       <div id="hands">
+            <header>
+                <h1>Silverjack</h1>
+            </header>
     
-    <?php
-    
-    include 'inc/functions.php';
-    
-    getHand();
-    
-    ?>
+        <?php
+            getHand();
+        ?>
     
 
     
-    </div>
-</body></html>
+        </div>
+    </body>
+</html>
